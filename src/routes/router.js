be96363a3f653c0ad42c092c55ed2d1c4f6b5670
erick-pacheco <linarespachecoerick@gmail.com>
2022-01-02 01:13:42 -0500

    Moves Switch to Routes in react-router-dom dependecy issue

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
+  Routes,
   useLocation,
-  useHistory
 } from "react-router-dom";
 import Header from "../Components/Header";
 import View from "../Components/View";
@@ -15,49 +14,33 @@ import Contact from "../Pages/Contact";
 import PrivacyPolicy from "../Pages/PrivacyPolicy";
 import Projects from "../Pages/Projects";
 
-// You can use the last <Route> in a <Switch> as a kind of
-// "fallback" route, to catch 404 errors.
-//
-// There are a few useful things to note about this example:
-//
-// - A <Switch> renders the first child <Route> that matches
-// - A <Redirect> may be used to redirect old URLs to new ones
-// - A <Route path="*> always matches
-
 export default function RenderRoutes() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <WelcomeHero />
-        </Route>
-        <Route path="/about">
-          <Header />
-          <About />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/contact" component={Contact} />
-        <Route path="/privacy" component={PrivacyPolicy} />
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<WelcomeHero />} />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+            </>
+          }
+        />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </Router>
   );
 }
 
 function NoMatch() {
   let location = useLocation();
-  let history = useHistory()
-  const [{ theme }] = useStateValue()
 
-  function delayandGo() {
-    setTimeout(() => history.push('/'), 3000)
-  }
-
-  useEffect(() => delayandGo(), [])
+  const [{ theme }] = useStateValue();
 
   return (
     <View>
@@ -68,13 +51,14 @@ function NoMatch() {
           minWidth: window.screen.availWidth,
         }}
       >
-        <p className={selectors.textColor(theme, 'mx-5 p-5 tiktok-link')}>No match for route &nbsp;
-          <code className='py-1 px-5 bg bg-light'>{location.pathname}</code>
+        <p className={selectors.textColor(theme, "mx-5 p-5 tiktok-link")}>
+          No match for route &nbsp;
+          <code className="py-1 px-5 bg bg-light">{location.pathname}</code>
         </p>
-        <p className={selectors.textColor(theme, 'mx-5')}>You'll return Home in 3 secs</p>
+        <p className={selectors.textColor(theme, "mx-5")}>
+          You'll return Home in 3 secs
+        </p>
       </div>
     </View>
   );
 }
-
-
